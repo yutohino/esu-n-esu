248f6a21fa21716dd33726d4a6ca5f0362ccaae0
Yuto Hino <yutohino48273@gmail.com>
2023-03-25 17:52:42 +0900

    アプリを縦向きの固定

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,17 @@ import 'package:esu_n_esu/home/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter/services.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(name: 'name', options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
